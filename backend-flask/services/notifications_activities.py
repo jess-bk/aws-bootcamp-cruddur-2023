b4ffd15ec34d2403dcbba453c26959a40c0528e7
jess-bk <victory187@hotmail.com>
2023-02-21 11:15:47 +0000

    created new backend notification api endpoint

---
+++ backend-flask/services/notifications_activities.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta, timezone
+class NotificationsActivities:
+  def run():
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'jess-bk',
+      'message': 'I am mern stack developer',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 1,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'andrew brown',
+        'message': 'wow amazing!',
+        'likes_count': 10,
+        'replies_count': 1,
+        'reposts_count': 1,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    }
+    ]
+    return results
