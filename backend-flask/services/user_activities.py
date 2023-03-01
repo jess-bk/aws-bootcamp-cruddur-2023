8f8f7049f59727500db7783f5417fc86a1cd23ed
jess-bk <victory187@hotmail.com>
2023-03-01 16:30:27 +0000

    update to stop logs fro aws xray and cloudwatch logs

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,12 +4,7 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
         # xray ---
-    segment = xray_recorder.begin_segment('user_activities')
-
-    model = {
-      'errors': None,
-      'data': None
-    }
+    # segment = xray_recorder.begin_segment('user_activities')
     
     model = {
       'errors': None,
@@ -31,11 +26,11 @@ class UserActivities:
       }]
       model['data'] = results
       
-    subsegment = xray_recorder.begin_subsegment('mock-data')
-    # xray ---
-    dict = {
-      "now": now.isoformat(),
-      "results-size": len(model['data'])
-    }
-    subsegment.put_metadata('key', dict, 'namespace')
+    # subsegment = xray_recorder.begin_subsegment('mock-data')
+    # # xray ---
+    # dict = {
+    #   "now": now.isoformat(),
+    #   "results-size": len(model['data'])
+    # }
+    # subsegment.put_metadata('key', dict, 'namespace')
     return model
