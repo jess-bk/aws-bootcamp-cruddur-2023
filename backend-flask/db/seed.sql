6774fe04be1750ce3b5f3e2a7b65b78d2e5ba0e5
jess-bk <victory187@hotmail.com>
2023-03-14 18:35:05 +0000

    update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,18 +1,13 @@
-#! /usr/bin/bash
+-- this file was manually created
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK');
 
-CYAN='\033[1;36m'
-NO_COLOR='\033[0m'
-LABEL="db-seed"
-printf "${CYAN}== ${LABEL}${NO_COLOR}\n"
-
-seed_path="$(realpath .)/db/seed.sql"
-echo $seed_path
-
-if [ "$1" = "prod" ]; then
-  echo "Running in production mode"
-  URL=$PROD_CONNECTION_URL
-else
-  URL=$CONNECTION_URL
-fi
-
-psql $URL cruddur < $seed_path
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
