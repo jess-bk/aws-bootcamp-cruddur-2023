d178184a6f9322d74d5b619ea2483d0d9e69305c
jess-bk <victory187@hotmail.com>
2023-03-16 14:36:37 +0000

    Added implementation of Lambda Function for Sign up

---
+++ aws/lambdas/cruddur-post-confirrmation.py
@@ -0,0 +1,48 @@
+import json
+import psycopg2
+import os
+
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    user_display_name = user['name']
+    user_email = user['email']
+    user_cognito_id = user['sub']
+    user_handle = user['preferred_username']
+
+    try:
+        
+        sql = f"""
+        INSERT INTO users (
+            display_name,
+            email,
+            handle, 
+            cognito_user_id
+            )
+        VALUES(
+            '{user_display_name}',
+            '{user_email}', 
+            '{user_handle}', 
+            '{user_cognito_id}'
+        )
+        """
+        
+        print('SQL =========')
+        print(sql)
+        
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+        
+        cur.execute(sql)
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
