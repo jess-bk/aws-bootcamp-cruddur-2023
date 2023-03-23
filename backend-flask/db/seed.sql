7eaedb7353d1f7620274942e77c82594dc7e7413
jess-bk <victory187@hotmail.com>
2023-03-23 00:54:04 +0000

    not receiving data in messages

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Jess BK', 'jess-bk' , 'jessbk@hotmail.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'test@test.com', 'MOCK');
+  ('Andrew Bayko', 'bayko' , 'test@test.com', 'MOCK'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- frontend-react-js/src/components/MessageGroupFeed.js
+++ frontend-react-js/src/components/MessageGroupFeed.js
@@ -1,15 +1,26 @@
-import './MessageGroupFeed.css';
-import MessageGroupItem from './MessageGroupItem';
-
+import "./MessageGroupFeed.css";
+import MessageGroupItem from "./MessageGroupItem";
+import MessageGroupNewItem from "./MessageGroupNewItem";
 export default function MessageGroupFeed(props) {
+  let message_group_new_item;
+  if (props.otherUser) {
+    message_group_new_item = <MessageGroupNewItem user={props.otherUser} />;
+  }
+
   return (
-    <div className='message_group_feed'>
-      <div className='message_group_feed_heading'>
-        <div className='title'>Messages</div>
+    <div className="message_group_feed">
+      <div className="message_group_feed_heading">
+        <div className="title">Messages</div>
       </div>
-      <div className='message_group_feed_collection'>
-        {props.message_groups.map(message_group => {
-        return  <MessageGroupItem key={message_group.uuid} message_group={message_group} />
+      <div className="message_group_feed_collection">
+        {message_group_new_item}
+        {props.message_groups.map((message_group) => {
+          return (
+            <MessageGroupItem
+              key={message_group.uuid}
+              message_group={message_group}
+            />
+          );
         })}
       </div>
     </div>
