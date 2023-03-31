c0809ddfe7456515629acfea75be099c619b49e7
jess-bk <74265740+jess-bk@users.noreply.github.com>
2023-03-31 15:58:59 +0000

    add nginx file and updated frontend for warnings and ran build frontend react

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -1,7 +1,7 @@
-import './ProfileInfo.css';
-import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
+import "./ProfileInfo.css";
+import { ReactComponent as ElipsesIcon } from "./svg/elipses.svg";
 import React from "react";
-import { Auth } from 'aws-amplify';
+import { Auth } from "aws-amplify";
 
 // [TODO] Authenication
 
@@ -9,40 +9,44 @@ export default function ProfileInfo(props) {
   const [popped, setPopped] = React.useState(false);
 
   const click_pop = (event) => {
-    setPopped(!popped)
-  }
+    setPopped(!popped);
+  };
 
   const signOut = async () => {
     try {
-        await Auth.signOut({ global: true });
-        window.location.href = "/"
-        localStorage.removeItem("access_token")
+      await Auth.signOut({ global: true });
+      window.location.href = "/";
+      localStorage.removeItem("access_token");
     } catch (error) {
-        console.log('error signing out: ', error);
+      console.log("error signing out: ", error);
     }
-  }
+  };
 
   const classes = () => {
     let classes = ["profile-info-wrapper"];
-    if (popped == true){
-      classes.push('popped');
+    if (popped === true) {
+      classes.push("popped");
     }
-    return classes.join(' ');
-  }
+    return classes.join(" ");
+  };
 
   return (
     <div className={classes()}>
       <div className="profile-dialog">
-        <button onClick={signOut}>Sign Out</button> 
+        <button onClick={signOut}>Sign Out</button>
       </div>
       <div className="profile-info" onClick={click_pop}>
         <div className="profile-avatar"></div>
         <div className="profile-desc">
-          <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
-          <div className="profile-username">@{props.user.handle || "handle"}</div>
+          <div className="profile-display-name">
+            {props.user.display_name || "My Name"}
+          </div>
+          <div className="profile-username">
+            @{props.user.handle || "handle"}
+          </div>
         </div>
-        <ElipsesIcon className='icon' />
+        <ElipsesIcon className="icon" />
       </div>
     </div>
-  )
+  );
 }
