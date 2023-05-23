bc6321aa7decbcea69eddf48fc293da75bab0907
jess-bk <victory187@hotmail.com>
2023-05-23 09:23:50 +0000

    fixed the activity form to not be hardcoded

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,6 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Jess BK','victory187@hotmail.com' , 'jess-bk' ,'MOCK'),
+  ('cloud dev','saimarshad143@hotmail.com' , 'cloud-dev' ,'MOCK'),
   ('Andrew Bayko','saimarshad143@hotmail.com' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
