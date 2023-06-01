7e29db03221ffd8ea55b1f98d27c7dfaeccdde64
jess-bk <victory187@hotmail.com>
2023-06-01 11:27:21 +0000

    fix frontend for message group page and fix compiling error for the frontend

--- frontend-react-js/src/pages/MessageGroupsPage.js
+++ frontend-react-js/src/pages/MessageGroupsPage.js
@@ -3,7 +3,8 @@ import React from "react";
 
 import DesktopNavigation from "../components/DesktopNavigation";
 import MessageGroupFeed from "../components/MessageGroupFeed";
-import { checkAuth, getAccessToken } from "../lib/CheckAuth";
+import { checkAuth } from "../lib/CheckAuth";
+import { get } from "lib/Requests";
 
 export default function MessageGroupsPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
@@ -12,25 +13,13 @@ export default function MessageGroupsPage() {
   const dataFetchedRef = React.useRef(false);
 
   const loadData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`;
-      await getAccessToken();
-      const access_token = localStorage.getItem("access_token");
-      const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-        method: "GET",
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setMessageGroups(resJson);
-      } else {
-        console.log(res);
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`;
+    get(url, {
+      auth: true,
+      success: function (data) {
+        setMessageGroups(data);
+      },
+    });
   };
 
   React.useEffect(() => {
@@ -41,7 +30,6 @@ export default function MessageGroupsPage() {
     loadData();
     checkAuth(setUser);
   }, []);
-
   return (
     <article>
       <DesktopNavigation user={user} active={"home"} setPopped={setPopped} />
